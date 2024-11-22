dd9b190f7ae14a8258f2644bb5622ac9f0471831
rakellbandeira <per22016l@byui.edu>
2024-11-21 22:16:54 -0400

    W03: Team Activity - Home Page Responsive Images

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -13,3 +13,30 @@ document.getElementById('curYear').textContent = currentYear;
 // get the document's last modified date
 const lastModifiedDate = document.lastModified;
 document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
+
+
+/* Visit Number dynamic */
+let visitBox = document.getElementById('visitParag');
+let currentDate = Date.now();
+let lastVisit = localStorage.getItem('lastVisit');
+let message = 'Welcome!';
+
+if (!lastVisit) {
+    
+    message = "Welcome! Let us know if you have any questions.";
+} else {
+    
+    let timeDifference = currentDate - lastVisit; 
+    let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); 
+
+    if (daysDifference < 1) {
+        message = "Back so soon! Awesome!";
+    } else if (daysDifference === 1) {
+        message = "You last visited 1 day ago.";
+    } else {
+        message = `You last visited ${daysDifference} days ago.`;
+    }
+}
+
+visitBox.textContent = message;
+localStorage.setItem('lastVisit', currentDate);
