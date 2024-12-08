749ffea28722d093ad4656bc8724df040b73c213
rakellbandeira <per22016l@byui.edu>
2024-12-08 02:55:56 -0400

    W05: Project Milestone - The Directory Page

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -67,3 +67,21 @@ let listContainer = document.querySelector('#list-container');
 }
 
 getLinks();
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#list-container");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
